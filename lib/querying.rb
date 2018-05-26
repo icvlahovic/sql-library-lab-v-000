83f2d91cd17a7627eeafd22974b8f8b846e24c1f
Ian Vlahovic <banddorkfht7@gmail.com>
2018-05-26 03:15:27 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -2,7 +2,7 @@ def select_books_titles_and_years_in_first_series_order_by_year
   "SELECT books.title, books.year
    FROM books
    JOIN series ON books.series_id = series.id
-   WHERE series.id = 1
+   WHERE series.id = (SELECT MIN(series.id) FROM series)
    ORDER BY books.year;"
 end
 
@@ -13,7 +13,9 @@ def select_name_and_motto_of_char_with_longest_motto
 end
 
 def select_value_and_count_of_most_prolific_species
-  "Write your SQL query here"
+  "SELECT characters.species, COUNT(characters.species)
+   FROM characters
+   "
 end
 
 def select_name_and_series_subgenres_of_authors
