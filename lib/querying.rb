09fa41d8023ad9729ae969cf356a68ff9b2dbb1c
Ian Vlahovic <banddorkfht7@gmail.com>
2018-05-26 03:35:53 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -39,5 +39,6 @@ def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name, COUNT(*) AS book_count
    FROM characters
    JOIN character_books on characters.id = character_books.character_id
+   JOIN books on character_books.book_id = books.id
    ORDER BY book_count DESC;"
 end
